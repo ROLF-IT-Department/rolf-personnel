8bc1404e798eec21d681b909709ddf55b4cb3c35
smgladkovskiy <smgladkovskiy@gmail.com>
2010-10-11 09:30:03 +0000

    Разделение тренингов по платформам (#3)

--- library/Rp/Db/Table/Ach/Trainings/GroupsMethods.php
+++ library/Rp/Db/Table/Ach/Trainings/GroupsMethods.php
@@ -14,12 +14,12 @@
  * @package    Rp_Db
  * @subpackage Rp_Db_Table
  */
-class Rp_Db_Table_Ach_Trainings_GroupsMethods extends Rp_Db_Table_Abstract 
+class Rp_Db_Table_Ach_Trainings_GroupsMethods extends Rp_Db_Table_Abstract
 {
 	protected $_name = 'user_rp_ach_trainings_groups_methods';
-	
+
 	protected $_dependentTables = array('Rp_Db_Table_Ach_Trainings_Methods');
-	
+
 	/**
 	 * Возвращает массив названий групп методов.
 	 *
@@ -29,7 +29,7 @@ class Rp_Db_Table_Ach_Trainings_GroupsMethods extends Rp_Db_Table_Abstract
 	{
 		return $this->_fetchPairs('name', null, 'sort, name');
 	}
-	
+
 	/**
 	 * Возвращает массив названий групп методов.
 	 *
@@ -37,12 +37,12 @@ class Rp_Db_Table_Ach_Trainings_GroupsMethods extends Rp_Db_Table_Abstract
 	 */
 	public function fetchDisabledNames()
 	{
-		return $this->_fetchPairs('name', 'disabled is null', 'sort, name');
+		return $this->_fetchPairs('name', 'disabled is null', 'sort, name', 'platform');
 	}
-	
+
 	/**
 	 * Возвращает ВСЕ названия групп и методов в виде массива.
-	 * Ключами этого массива являются названия групп методов, 
+	 * Ключами этого массива являются названия групп методов,
 	 * а значениями - массивы названий методов соответствующей группы.
 	 *
 	 * @return array
@@ -52,7 +52,7 @@ class Rp_Db_Table_Ach_Trainings_GroupsMethods extends Rp_Db_Table_Abstract
 		$groups = $this->fetchNames();
 		$methods = new Rp_Db_Table_Ach_Trainings_Methods();
 		$methods = $methods->fetchAll(null, array('sort', 'name'))->toArray();
-		
+
 		foreach ($methods as $item) {
 			$group =& $groups[$item['group_id']];
 			if (!is_array($group)) {
@@ -69,23 +69,23 @@ class Rp_Db_Table_Ach_Trainings_GroupsMethods extends Rp_Db_Table_Abstract
 				$groupsNames[$item['name']] = $item['methods'];
 			}
 		}
-		
+
 		return $groupsNames;
 	}
-	
+
 	/**
 	 * Возвращает ТОЛЬКО АКТУАЛЬНЫЕ названия групп и методов в виде массива.
-	 * Ключами этого массива являются названия групп методов, 
+	 * Ключами этого массива являются названия групп методов,
 	 * а значениями - массивы названий методов соответствующей группы.
 	 *
 	 * @return array
 	 */
-	public function toArrayNamesWithoutDisabled()
+	public function toArrayNamesWithoutDisabled($platform = NULL)
 	{
 		$groups = $this->fetchDisabledNames();
 		$methods = new Rp_Db_Table_Ach_Trainings_Methods();
-		$methods = $methods->fetchAll('disabled is null', array('sort', 'name'))->toArray();
-		
+		$methods = $methods->fetchAll(array('disabled is null', 'platform = \''.$platform.'\''), array('sort', 'name'))->toArray();
+
 		foreach ($methods as $item) {
 			$group =& $groups[$item['group_id']];
 			if (!is_array($group)) {
@@ -102,7 +102,7 @@ class Rp_Db_Table_Ach_Trainings_GroupsMethods extends Rp_Db_Table_Abstract
 				$groupsNames[$item['name']] = $item['methods'];
 			}
 		}
-		
+
 		return $groupsNames;
 	}
 }
