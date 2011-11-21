9da744b825f098769f6db44a32ee65554fe3893c
smgladkovskiy <SMGladkovskiy@gmail.com>
2011-11-21 12:32:31 +0400

    Активирование дебагбара + кэширование запросов на исследование структуры таблиц (- ~ 100 запросов к БД)

--- wwwroot/index.php
+++ wwwroot/index.php
@@ -9,8 +9,25 @@ Zend_Loader::registerAutoload();
 ini_set('display_errors', Rp::getConfig('system')->debug_mode);
 
 Zend_Controller_Action_HelperBroker::addHelper(new Rp_Controller_Action_Helper_ViewRenderer());
+$cache = Zend_Cache::factory('Core', 'Memcached', array('lifetime' => 7200, 'automatic_serialization' => TRUE));
+Zend_Db_Table_Abstract::setDefaultMetadataCache($cache);
+
+$options = array(
+    'plugins' => array('Variables',
+		'Html',
+		'Database' => array('adapter' => array('standard' => Rp::getDbAdapter())),
+		'File' => array('basePath' => '../application'),
+		'Memory',
+		'Time',
+		'Registry',
+		'Cache' => array('backend' => $cache->getBackend()),
+		'Exception')
+);
+
+
 
 Zend_Controller_Front::getInstance()
 	->registerPlugin(new Rp_Controller_Plugin_System())
+	->registerPlugin(new ZFDebug_Controller_Plugin_Debug($options))
 	->addModuleDirectory('../application')
 	->dispatch();
